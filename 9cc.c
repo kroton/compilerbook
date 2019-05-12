6d10611835918511b46f616f52a02d565e8aae5e
kroton <kroton.prog@gmail.com>
2019-05-12 15:40:07 +0900

    単項演算子に対応

--- 9cc.c
+++ 9cc.c
@@ -92,6 +92,7 @@ Node *new_node_num(int val) {
 }
 
 Node *term();
+Node *unary();
 Node *mul();
 
 Node *add() {
@@ -109,17 +110,27 @@ Node *add() {
 }
 
 Node *mul() {
-  Node *node = term();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*')) {
-      node = new_node('*', node, term());
+      node = new_node('*', node, unary());
     } else if (consume('/')) {
-      node = new_node('/', node, term());
+      node = new_node('/', node, unary());
     } else {
       return node;
     }
   }
+}
+
+Node *unary() {
+  if (consume('+')) {
+    return term();
+  }
+  if (consume('-')) {
+    return new_node('-', new_node_num(0), term());
+  }
+  return term();
 }
 
 Node *term() {
