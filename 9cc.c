d00b20884f64a0455660d8ca55877b07559b022e
kroton <kroton.prog@gmail.com>
2019-05-12 15:15:49 +0900

    加減算をASTとスタックマシン実装に変更

--- 9cc.c
+++ 9cc.c
@@ -4,6 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 enum {
   TK_NUM = 256, // 整数トークン
   TK_EOF,       // 入力終わりを表すトークン
@@ -16,14 +24,7 @@ typedef struct {
 } Token;
 
 Token tokens[100];
-
-void error(char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
+int pos;
 
 void tokenize(char *p) {
   int i = 0;
@@ -57,45 +58,96 @@ void tokenize(char *p) {
   tokens[i].input = p;
 }
 
+int consume(int ty) {
+  if (tokens[pos].ty != ty)
+    return 0;
+  pos++;
+  return 1;
+}
+
+enum {
+  ND_NUM = 256, // 整数のノードの型
+};
+
+typedef struct Node {
+  int ty;           // 演算子かND_NUM
+  struct Node* lhs; // 左辺
+  struct Node* rhs; // 右辺
+  int val;          // tyがND_NUMの場合のみ使う
+} Node;
+
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *add() {
+  if (tokens[pos].ty != TK_NUM) {
+    error("最初の項が数ではありません");
+  }
+
+  Node *node = new_node_num(tokens[pos++].val);
+
+  for (;;) {
+    if (consume('+')) {
+      node = new_node('+', node, add());
+    } else if (consume('-')) {
+      node = new_node('-', node, add());
+    } else {
+      return node;
+    }
+  }
+}
+
+void gen(Node *node) {
+  if (node->ty == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty) {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
   tokenize(argv[1]);
-  char *p = argv[1];
+  Node *node = add();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  if (tokens[0].ty != TK_NUM) 
-    error("最初の項が数ではありません");
-  printf("  mov rax, %d\n", tokens[0].val);
+  gen(node);
 
-  int i = 1;
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-        error("予期しないトークンです: %s", tokens[i].input);
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-        error("予期しないトークンです: %s", tokens[i].input);
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    error("予期しないトークンです: %s", tokens[i].input);
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
